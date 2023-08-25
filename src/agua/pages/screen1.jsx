ab694b75912849c3b4509032ac612b5e7d2d8651
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-25 11:57:59 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+
+
+
+function Screen1() {
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<div id="layer-1-73cd5669a7324d7ab8c18d3f28e12248" className={styles["layer-1-73cd5669a7324d7ab8c18d3f28e12248"]} >
+<div id="layer-2-eecf1cd2cff74ca693bdef4e8f0324a4" className={styles["layer-2-eecf1cd2cff74ca693bdef4e8f0324a4"]} >
+<img id="imageview-1-8996587f6e994ee4a793513fe034ca30" className={styles["imageview-1-8996587f6e994ee4a793513fe034ca30"]} alt="imageview-1" src='/assets/1.apple-logo-black.png' />
+</div>
+<div id="layer-3-4f72c864f97f4df095749e1fd44b8083" className={styles["layer-3-4f72c864f97f4df095749e1fd44b8083"]+ ' layer-3-4f72c864f97f4df095749e1fd44b8083-orientation'} >
+<div id="layer-5-0f0788c234614021bd6f2faf19dfa43d" className={styles["layer-5-0f0788c234614021bd6f2faf19dfa43d"]} >
+<div id="text-12-1c625e0670834f41b65fdd617133f288" className={styles["text-12-1c625e0670834f41b65fdd617133f288"]} >Store</div>
+</div>
+<div id="layer-6-ca97b3e9eda1460db0d80ea58dedf32d" className={styles["layer-6-ca97b3e9eda1460db0d80ea58dedf32d"]} >
+<div id="text-2-f481dfdf7d8f4139955f996f191ae1cc" className={styles["text-2-f481dfdf7d8f4139955f996f191ae1cc"]} >Mac</div>
+<div id="text-3-fcd5f6b8d2f94c2ea56000bc3624f030" className={styles["text-3-fcd5f6b8d2f94c2ea56000bc3624f030"]} >iPad</div>
+<div id="text-4-dad8ab6df0df44509698018756bd1fdf" className={styles["text-4-dad8ab6df0df44509698018756bd1fdf"]} >iPhone</div>
+</div>
+<div id="layer-4-8223169ed4334a909eaa803bbbb6ed07" className={styles["layer-4-8223169ed4334a909eaa803bbbb6ed07"]} >
+<div id="text-5-7315e9d9c6324b368c5fb50ba055a6ac" className={styles["text-5-7315e9d9c6324b368c5fb50ba055a6ac"]} >Watch</div>
+<div id="text-6-4067b37b5eb444c898caa4f9d904d016" className={styles["text-6-4067b37b5eb444c898caa4f9d904d016"]} >Vision</div>
+<div id="text-7-de89a4c229fb4e898e0f0f339a10a702" className={styles["text-7-de89a4c229fb4e898e0f0f339a10a702"]} >AirPods</div>
+</div>
+<div id="layer-8-a429557f4d4f460cb935b60cfe721e88" className={styles["layer-8-a429557f4d4f460cb935b60cfe721e88"]} >
+<div id="text-8-8c82c8d2291c4fae9023c43f278a1a73" className={styles["text-8-8c82c8d2291c4fae9023c43f278a1a73"]} >TV & Home</div>
+<div id="text-9-e2a26c9c54c84d09bc106a94ea8ebb36" className={styles["text-9-e2a26c9c54c84d09bc106a94ea8ebb36"]} >Entertainment</div>
+<div id="text-10-cd9b5f84ee354ccb914408bbf4ae69bc" className={styles["text-10-cd9b5f84ee354ccb914408bbf4ae69bc"]} >Accessories</div>
+</div>
+<div id="layer-9-61b6171640d344e59997dafb99a2c6ee" className={styles["layer-9-61b6171640d344e59997dafb99a2c6ee"]} >
+<div id="text-11-a63d8432d0c94d29862ead5193de862e" className={styles["text-11-a63d8432d0c94d29862ead5193de862e"]} >Support</div>
+</div>
+</div>
+<div id="layer-7-61fe178683604c6b88a90ea3ec437a34" className={styles["layer-7-61fe178683604c6b88a90ea3ec437a34"]} >
+<span id="1739a37fe90343468bd35dd67da4396f" className={styles["icon-1-1739a37fe90343468bd35dd67da4396f"]} >search</span>
+<span id="4dcc6a36a23748ca9564ad40ac87cb84" className={styles["icon-2-4dcc6a36a23748ca9564ad40ac87cb84"]} >shopping_bag</span>
+</div>
+</div>
+<div id="layer-12-77984808dd5749a1a1892627faf1eb19" className={styles["layer-12-77984808dd5749a1a1892627faf1eb19"]} >
+<div id="layer-10-d1db24a6238b40bebfe6904016003d3f" className={styles["layer-10-d1db24a6238b40bebfe6904016003d3f"]} ></div>
+<div id="layer-11-e76aa83191514abb913003a2e3f95bf9" className={styles["layer-11-e76aa83191514abb913003a2e3f95bf9"]} ></div>
+<div id="layer-13-9b4e8027346b4a0f8329868b792797ef" className={styles["layer-13-9b4e8027346b4a0f8329868b792797ef"]} ></div>
+<div id="layer-14-63a313eae2584e5d872ef9468d9a3b2d" className={styles["layer-14-63a313eae2584e5d872ef9468d9a3b2d"]} ></div>
+</div>
+        </div>
+    );
+}
+
+export default Screen1;
+            
